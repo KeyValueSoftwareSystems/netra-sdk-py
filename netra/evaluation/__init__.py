ab0fbd50ec1f53459d246e8093f55186cdb90a7f
akash-vijay-kv <akash@keyvalue.systems>
2026-01-07 13:47:19 +0530

    chore: modify BaseEvaluator structure

--- netra/evaluation/__init__.py
+++ netra/evaluation/__init__.py
@@ -1,5 +1,13 @@
 from netra.evaluation.api import Evaluation
 from netra.evaluation.evaluator import BaseEvaluator
-from netra.evaluation.models import DatasetItem, EvaluatorConfig, EvaluatorOutput
+from netra.evaluation.models import DatasetItem, EvaluatorConfig, EvaluatorContext, EvaluatorOutput, ScoreType
 
-__all__ = ["Evaluation", "DatasetItem", "BaseEvaluator", "EvaluatorOutput", "EvaluatorConfig"]
+__all__ = [
+    "Evaluation",
+    "DatasetItem",
+    "BaseEvaluator",
+    "EvaluatorContext",
+    "EvaluatorOutput",
+    "EvaluatorConfig",
+    "ScoreType",
+]
