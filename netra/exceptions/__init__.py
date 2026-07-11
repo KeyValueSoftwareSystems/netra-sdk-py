43f941a368c6a9a46978b1933cf04d9303b4fee1
akash-vijay-kv <akash@keyvalue.systems>
2026-07-11 09:05:17 +0530

    refactor: Remove pii detection & prompt injection detection modules

--- netra/exceptions/__init__.py
+++ netra/exceptions/__init__.py
@@ -1,6 +1,5 @@
 # File: netra/exceptions/__init__.py
 
 from .injection import InjectionException
-from .pii import PIIBlockedException
 
-__all__ = ["PIIBlockedException", "InjectionException"]
+__all__ = ["InjectionException"]
